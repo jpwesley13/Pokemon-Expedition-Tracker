aaff8185b84ef2ff2f891cd555e0ea2c46ac06e9
Jared Patterson <jpwesley34@gmail.com>
2024-10-10 15:15:35 -0700

    Add additional models and relationships.

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
 
 from config import db, bcrypt
@@ -29,7 +30,7 @@ class Pokedex(db.Model, SerializerMixin):
 
     user = db.relationship('User', back_populates='pokedex')
 
-    serialize_rules = ('-users.pokedex')
+    serialize_rules = ('-users.pokedex',)
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -41,4 +42,53 @@ class User(db.Model, SerializerMixin):
 
     pokedex = db.relationship('Pokedex', uselist=False, back_populates='user')
 
-    serialize_rules = ('-pokedexes.user')
+    serialize_rules = ('-pokedexes.user',)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Please enter a name.')
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if age < 10:
+            raise ValueError('Trainers must be at least 10 years old.')
+        return age
+
+    def __repr__(self):
+        return f'<User {self.id}: {self.username}, age: {self.age}>'
+    
+class Locale(db.Model, SerializerMixin):
+    __tablename__ = 'locales'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('regions.id'))
+
+    region = db.relationship('Region', back_populates='locales')
+
+    serialize_rules = ('-region.locales',)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Habitat must be named.')
+        elif 25 < len(name) < 2:
+            raise ValueError('Habitat names must be between 2-25 characters long')
+        return name
+
+    def __repr__(self):
+        return f'<Habitat {self.id}: {self.name}>'
