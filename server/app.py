e0ffc7cb7a68788079b7e97a85a3dfea63b10e17
Jared Patterson <jpwesley34@gmail.com>
2024-10-12 13:57:18 -0700

    Add views to app.py.

--- server/app.py
+++ server/app.py
@@ -1,21 +1,199 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import request, make_response, session
 from flask_restful import Resource
+from models import *
+from sqlalchemy.exc import IntegrityError
 
-# Local imports
+
 from config import app, db, api
-# Add your model imports
-
-
-# Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class Regions(Resource):
+    def get(self):
+        regions = [region.to_dict() for region in Region.query.all()]
+        return make_response(regions, 200)
+    
+    def post(self):
+        params = request.get_json()
+
+        try:
+            new_region = Region(
+                name = params['name'],
+            )
+            db.session.add(new_region)
+            db.session.commit()
+            return make_response(new_region.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class RegionById(Resource):
+    def get(self, id):
+        region = Region.query.filter(Region.id == id).first()
+        if region:
+            return make_response(region.to_dict(), 200)
+        return make_response({'error': 'Region not found.'}, 404)
+    
+class Types(Resource):
+    def get(self):
+        types = [type.to_dict() for type in Type.query.all()]
+        return make_response(types, 200)
+    
+    def post(self):
+        params = request.get_json()
+
+        try:
+            new_type = Type(
+                name = params['name']
+            )
+            db.session.add(new_type)
+            db.session.commit()
+            return make_response(new_type, 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class TypeById(Resource):
+    def get(self, id):
+        type = Type.query.filter(Type.id == id).first()
+        if type:
+            return make_response(type.to_dict(), 200)
+        return make_response({'error': 'Type not found.'}, 404)
+    
+class Pokedexes(Resource):
+    def get(self):
+        pokedexes = [pokedex.to_dict() for pokedex in Pokedex.query.all()]
+        return make_response(pokedexes, 200)
+    
+    def post(self):
+        params = request.get_json()
+
+        try:
+            new_pokedex = Pokedex(
+                user_id = params['user_id']
+            )
+            db.session.add(new_pokedex)
+            db.session.commit()
+            return make_response(new_pokedex.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class PokedexById(Resource):
+    def get(self, id):
+        pokedex = Pokedex.query.filter(Pokedex.id == id).first()
+        if pokedex:
+            return make_response(pokedex.to_dict(), 200)
+        return make_response({'error': 'Pokedex not found.'}, 404)
+    
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(users, 200)
+    
+class UserById(Resource):
+    def get(self, id):
+        user = User.query.filter(User.id == id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({'error': 'User not found.'}, 404)
+    
+    def patch(self, id):
+        user = User.query.filter(User.id == id).first()
+        params = request.get_json()
+
+        if user is None:
+            return make_response({'error': 'User not found.'}, 404)
+        
+        try:
+            for attr in params:
+                if attr == "username" and User.query.filter(User.username == params[attr]).first():
+                    return make_response({"errors": {"username": "Username must be unique"}}, 400)
+                setattr(user, attr, params[attr])
+            db.session.commit()
+            return make_response(user.to_dict(), 202)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class Locales(Resource):
+    def get(self):
+        locales = [locale.to_dict() for locale in Locale.query.all()]
+        return make_response(locales, 200)
+    
+    def post(self):
+        params = request.get_json()
+
+        try:
+            new_locale = Locale(
+                name = params['name'],
+                region_id = params['region_id']
+            )
+            db.session.add(new_locale)
+            db.session.commit()
+            return make_response(new_locale.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class LocaleById(Resource):
+    def get(self, id):
+        locale = Locale.query.filter(Locale.id == id).first()
+        if locale:
+            return make_response(locale.to_dict(), 200)
+        return make_response({'error': 'Locale not found.'}, 404)
+    
+class Goals(Resource):
+    def get(self):
+        goals = [goal.to_dict() for goal in Goal.query.all()]
+        return make_response(goals, 200)
+    
+    def post(self):
+        params = request.get_json()
+
+        try:
+            new_goal = Goal(
+                content = params['content'],
+                target_date = params['target_date'],
+                user_id = params['user_id']
+            )
+            db.session.add(new_goal)
+            db.session.commit()
+            return make_response(new_goal.to_dict(), 201)
+        except (ValueError):
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class GoalById(Resource):
+    def get(self, id):
+        goal = Goal.query.filter(Goal.id == id).first()
+        if goal:
+            return make_response(goal.to_dict(), 200)
+        return make_response({'error': 'Goal not found.'}, 404)
+    
+    def patch(self, id):
+        goal = Goal.query.filter(Goal.id == id).first()
+        params = request.get_json()
+
+        if goal is None:
+            return make_response({"error": "Goal not found."}, 404)
+        
+        try:
+            for attr in params:
+                setattr(goal, attr, params[attr])
+            db.session.commit()
+            return make_response(goal.to_dict(), 202)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+    
+    def delete(self, id):
+        goal = Goal.query.filter(Goal.id == id).first()
+
+        if goal is None:
+            return make_response({"error": "Goal not found."}, 404)
+        
+        db.session.delete(goal)
+        db.session.commit()
+
+        return {}, 204
 
 
 if __name__ == '__main__':
