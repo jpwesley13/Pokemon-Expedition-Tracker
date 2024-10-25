57b5ce6d11256857243b13bfc6e23a122b983907
Jared Patterson <jpwesley34@gmail.com>
2024-10-25 14:53:24 -0700

    Add basic navbar and header.

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../context and hooks/AuthContext";
+
+function NavBar() {
+
+    const navigate = useNavigate();
+    const { user, setUser } = useAuth();
+
+    function handleLogout() {
+        fetch('/logout', {
+            method: 'DELETE'
+        })
+        .then(() => {
+            setUser(null);
+            navigate('/');
+        })
+    }
+
+    return (
+        <nav className="navbar">
+            <div className="navbutton">
+                <NavLink
+                to="/"
+                >
+                    Home
+                </NavLink>
+            </div>
+            <div className="navlogin">
+            {user ? (
+                <button onClick={handleLogout}>
+                    Logout
+                </button>
+                ) : (
+                <>
+                    <NavLink to="/signup">Signup</NavLink>
+                    <NavLink to="/login">Login</NavLink>
+                </>
+            )}
+        </div>
+    </nav>
+);
+};
+
+export default NavBar;
