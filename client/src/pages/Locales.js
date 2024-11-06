338c2a92d358ebf9424cccd46dbbb351493df430
Jared Patterson <jpwesley34@gmail.com>
2024-11-06 13:17:32 -0800

    Build out Locales page.

--- client/src/pages/Locales.js
+++ client/src/pages/Locales.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
+import LocaleCard from "../components/LocaleCard";
 import SortCard from "../components/SortCard";
 import Search from "../components/Search";
 import FilterCard from "../components/FilterCard";
 import { useAuth } from "../context and hooks/AuthContext";
+import ModalButton from "../components/ModalButton";
+import { Modal } from "@mui/material";
 
 function Locales() {
 
     const { user } = useAuth();
     const [locales, setLocales] = useState([]);
-    const [expeditions, setExpeditions] = useState([]);
+    // const [expeditions, setExpeditions] = useState([]);
     const [search, setSearch] = useState("");
     const [sortBy, setSortBy] = useState("Alphabetical Order");
     const [filterBy, setFilterBy] = useState("");
@@ -19,10 +22,10 @@ function Locales() {
         .then(res => res.json())
         .then(data => setLocales(data))
         .catch(error => console.error(error))
-        fetch(`/expeditions`)
-        .then(res => res.json())
-        .then(data => setExpeditions(data))
-        .catch(error => console.error(error))
+        // fetch(`/expeditions`)
+        // .then(res => res.json())
+        // .then(data => setExpeditions(data))
+        // .catch(error => console.error(error))
     }, [])
 
     function onAddLocales(newLocale){
@@ -42,4 +45,66 @@ function Locales() {
     const options = ["Alphabetical Order", "Most Expeditions"]
 
     const filteredLocales = sortedLocales.filter(locale => locale.region.name.includes(filterBy))
-}
+
+    const displayedLocales = filteredLocales.map(locale => (
+        <LocaleCard
+        key={locale.id}
+        locale={locale}
+        />
+    ))
+
+    const regions = [...new Set(locales.map(locale => locale.region.name))]
+
+    return (
+        <>
+            <hr/>
+            <h2 className="header">Locales:</h2>
+            <br />
+            <div className="search-container">
+                <Search
+                    search={search}
+                    searchSetter={setSearch}
+                />
+            </div>
+            <div className="filter-sort-container">
+                <div className="filter-sort-row">
+                    <FilterCard
+                        specifics={regions}
+                        label="locales"
+                        filterAttr="region"
+                        onChangeFilter={setFilterBy}
+                        filterCriteria={filterBy}
+                    />
+                    <SortCard
+                    sortBy={sortBy}
+                    onChangeSort={setSortBy}
+                    options={options}
+                    />
+                </div>
+                {user && ( <div className="filter-sort-button">
+                    <ModalButton variant="contained" color="primary" onClick={() => setIsModalOpen(true)}>
+                        Add new locale
+                    </ModalButton>
+                    </div>
+                )}
+                </div>
+                <div className="cards-container">
+                    {displayedLocales}
+                </div>
+                <Modal
+                    open={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                    aria-labelledby="edit-profile-modal-title"
+                    aria-describedby="edit-profile-modal-description"
+                >
+                    <Box className="modal-box">
+                        <h2>Add new locale</h2>
+                        <ModalButton className="close-button" onClick={() => setIsModalOpen(false)} sx={{ mb: 2 }}>Close</ModalButton>
+                        
+                    </Box>
+                </Modal>
+        </>
+    );
+};
+
+export default Locales;
