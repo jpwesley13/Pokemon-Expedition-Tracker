54a69a7c308bff141ecdbdcc707efb917c4c7fa1
Jared Patterson <jpwesley34@gmail.com>
2024-11-06 12:25:03 -0800

    Begin building out Locales page.

--- client/src/pages/Locales.js
+++ client/src/pages/Locales.js
@@ -1,1 +1,28 @@
 import { useState, useEffect } from "react";
+import SortCard from "../components/SortCard";
+import { useAuth } from "../context and hooks/AuthContext";
+
+function Locales() {
+
+    const { user } = useAuth();
+    const [locales, setLocales] = useState([]);
+    const [expeditions, setExpeditions] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        fetch(`/locales`)
+        .then(res => res.json())
+        .then(data => setLocales(data))
+        .catch(error => console.error(error))
+        fetch(`/expeditions`)
+        .then(res => res.json())
+        .then(data => setExpeditions(data))
+        .catch(error => console.error(error))
+    }, [])
+
+    function onAddLocales(newLocale){
+        return setLocales([...locales, newLocale])
+    }
+
+    
+}
