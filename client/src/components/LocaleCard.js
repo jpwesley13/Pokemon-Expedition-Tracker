c3ab84ef1ab685970e69c64c4a6f1f5c2d762e9e
Jared Patterson <jpwesley34@gmail.com>
2024-11-06 15:14:09 -0800

    Add expedition count and region identifiers for name-sharing locales.

--- client/src/components/LocaleCard.js
+++ client/src/components/LocaleCard.js
@@ -2,11 +2,16 @@ import { Link } from "react-router-dom";
 
 function LocaleCard({locale}) {
 
-    const {name, id} = locale
+    const {name, region, expeditions, id} = locale
+
+    const expeditionCount = expeditions.length
 
     return (
         <div className="card">
             <h2>{name}</h2>
+            <span>{region.name} Region</span>
+            <span>{expeditionCount} recorded expeditions</span>
+            <br />
             <Link to={`/locales/${id}`}>Details</Link>
         </div>
     );
