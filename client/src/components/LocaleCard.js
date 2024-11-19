496eaefcbbd35e6b880cae28cf3377c66542d03c
Jared Patterson <jpwesley34@gmail.com>
2024-11-19 02:09:22 -0800

    Incorporate most commonly caught species at each locale on Locale page.

--- client/src/components/LocaleCard.js
+++ client/src/components/LocaleCard.js
@@ -6,13 +6,20 @@ function LocaleCard({locale, catches = []}) {
 
     const expeditionCount = expeditions.length
 
-    
+    const speciesCount = catches.reduce((acc, capture) => {
+        const speciesName = capture.species.name;
+        acc[speciesName] = (acc[speciesName] || 0) + 1;
+        return acc;
+    }, {});
+
+    const mostCommon = Object.keys(speciesCount).reduce((a, b) => speciesCount[a] > speciesCount[b] ? a : b, "None")
 
     return (
         <div className="card">
             <h2>{name}</h2>
-            <span>{region.name} Region {catches.length}</span>
+            <span>{region.name} Region</span>
             <span>{expeditionCount} recorded expeditions</span>
+            <span>Most Common Pokemon Caught: {mostCommon}</span>
             <br />
             <Link to={`/locales/${id}`}>Details</Link>
         </div>
