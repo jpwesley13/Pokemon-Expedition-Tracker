49a26b1cdbb5d32de01e2f9cb4c603bf129d8eb7
Jared Patterson <jpwesley34@gmail.com>
2024-11-23 06:39:02 -0800

    Add individual Pokemon details to ExpeditionDetails.

--- client/src/components/ExpeditionDetails.js
+++ client/src/components/ExpeditionDetails.js
@@ -9,13 +9,40 @@ function ExpeditionDetails({ handleClick, pokemons = [], catchCount, typeCount }
         </span>
     ))
 
+    const expeditionPokemon = pokemons.map(pokemon => {
+        const types = pokemon.types.map((typeObj) =>  typeObj.name);
+        return (
+            <div key={pokemon.id} className="pokemon-entry">
+                {pokemon.name}: {types.map((type, i) => (
+                    <React.Fragment key={type}>
+                        <span 
+                        style={{ marginLeft: i === 0 ? '0.5em' : '0' }}
+                        className={`type-${type.toLowerCase()}`}>
+                        {type}
+                        </span>
+                        {i < types.length - 1 && <strong> / </strong>}
+                    </React.Fragment>
+                ))}
+            </div>
+        )
+    })
+
     return (
         <>
             <h2>{catchCount > 0 ? `You caught ${catchCount} Pokemon on this expedition!` : `You didn't catch any Pokemon on this expedition...`}</h2>
-            <h3>{expeditionTypes.length > 0 ? `Number of each Type caught:` : null}</h3>
-                <div className="types-container">
+            <h3>
+                {expeditionTypes.length > 0 ? `Number of each Type caught:` : null}
+            </h3>
+            <div className="types-container">
                 {expeditionTypes}
-                </div>
+            </div>
+            <hr />
+            <h3>
+                {expeditionPokemon.length > 0 ? `Pokemon caught on expedition:` : null}
+            </h3>
+            <div className="pokemon-container">
+                {expeditionPokemon}
+            </div>
             <button onClick={() => handleClick()}>Close</button>
         </>
     )
