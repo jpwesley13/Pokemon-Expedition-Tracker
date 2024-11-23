a9c852957df1c79f363cabd4180245320aadbc00
Jared Patterson <jpwesley34@gmail.com>
2024-11-23 05:49:46 -0800

    Add display for type count caught on expedition in details popup.

--- client/src/components/ExpeditionDetails.js
+++ client/src/components/ExpeditionDetails.js
@@ -1,11 +1,21 @@
+import React from "react";
 
+function ExpeditionDetails({ handleClick, pokemons = [], catchCount, typeCount }) {
 
-function ExpeditionDetails({ handleClick, pokemons, catchCount, typeCount }) {
-
+    const expeditionTypes = Object.entries(typeCount).map(([type, count]) => (
+        <span key={type}
+        className={`type-${type.toLowerCase()}`}>
+            {type}: {count}
+        </span>
+    ))
 
     return (
         <>
-            <p>You caught {catchCount} Pokemon</p>
+            <h2>{catchCount > 0 ? `You caught ${catchCount} Pokemon on this expedition!` : `You didn't catch any Pokemon on this expedition...`}</h2>
+            <h3>{expeditionTypes.length > 0 ? `Number of each Type caught:` : null}</h3>
+                <div className="types-container">
+                {expeditionTypes}
+                </div>
             <button onClick={() => handleClick()}>Close</button>
         </>
     )
