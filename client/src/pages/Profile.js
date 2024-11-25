0f0b567b8bee987904cf76349d4238813a0cf94c
Jared Patterson <jpwesley34@gmail.com>
2024-11-25 03:31:20 -0800

    Add various stats to user profile page through ternary displays to accommodate new users without stats.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { useParams, useNavigate, NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuth } from "../context and utility/AuthContext";
+import getMostCommon from "../context and utility/getMostCommon";
 
 function Profile() {
     const navigate = useNavigate();
@@ -15,6 +17,16 @@ function Profile() {
 
     console.log(userLocales)
 
+    const typeCount = catches.reduce((acc, capture) => {
+        capture.species.types.forEach((typeObj) => {
+            const speciesType = typeObj.name;
+            acc[speciesType] = (acc[speciesType] || 0) + 1;
+        });
+        return acc;
+    }, {});
+    
+    const mostCommon = getMostCommon(typeCount)
+
     useEffect(() => {
         if(id !== userId){
             navigate(`/users/${id}`)
@@ -27,6 +39,9 @@ function Profile() {
     return (
         <>
             <h1>Hey how's it growin'?</h1>
+            <h2>
+                {catches.length > 0 ? `You've caught ${catches.length} Pokemon across ${expeditions.length} expeditions!` : `Your Pokemon adventure is just getting started! Get out there and start catchin'!`}
+            </h2>
             <h3>
                 {userLocales.length > 0 ? `Locales you've visited on your expeditions:` : null}
             </h3>
@@ -37,6 +52,25 @@ function Profile() {
                     </div>
                 )}
             </div>
+            <br />
+            <h3>
+                {mostCommon.length > 0 ? `Most Common Type(s) You've Caught:` : null}
+            </h3>
+            <div>
+                    {
+                        mostCommon.map((type, i) => (
+                            <React.Fragment key={type}>
+                                <span 
+                                style={{ marginLeft: i === 0 ? '0.5em' : '0' }}
+                                className={`type-${type.toLowerCase()}`}>
+                                {type}
+                                </span>
+                                {i < mostCommon.length - 1 && <strong> / </strong>}
+                            </React.Fragment>
+                        ))
+                    }
+            </div>
+                <br />
             <NavLink to={`/users/${id}/pokedex`}>Pokedex</NavLink>
         </>
     );
