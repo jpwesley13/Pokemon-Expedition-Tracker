ec5c70a8d6a029c37518499abbcaf6fc59ba04d1
Jared Patterson <jpwesley34@gmail.com>
2024-12-01 03:39:40 -0800

    Give header a subtitle to explain app name.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 
 function Header() {
     return (
-        <><header className="header">
+        <>
+        <header className="header">
             <h1 className="headertitle">
                 PokET
             </h1>
+            <h2 className="headersubtitle">
+                The Pokémon Expedition Tracker
+            </h2>
+            <hr/>
         </header>
-        <hr/></>
+        <hr />
+        </>
     )
 }
 
