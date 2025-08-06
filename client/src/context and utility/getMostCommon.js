246f3b07006304589325dcc0538cb813987a34b4
Jared Patterson <jpwesley34@gmail.com>
2025-08-05 17:05:00 -0700

    Begin Typescript Conversion.

--- client/src/context and utility/getMostCommon.js
+++
@@ -1,6 +0,0 @@
-function getMostCommon(count) {
-    const maxCount = Math.max(...Object.values(count));
-    return Object.keys(count).filter(type => count[type] === maxCount);
-}
-
-export default getMostCommon;
--- client/src/context and utility/globalTime.js
+++
@@ -1,6 +0,0 @@
-function globalTime(dateString) {
-    const date = new Date(dateString);
-    return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
-};
-
-export default globalTime;
