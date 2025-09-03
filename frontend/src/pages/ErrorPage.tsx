536630ead28f2e037ce461940269313cdd8b5b1d
Jared Patterson <jpwesley34@gmail.com>
2025-09-03 08:27:52 -0700

    Remove unnecessary HTML elements from ErrorPage.

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -2,43 +2,38 @@ import { useRouteError, NavLink } from "react-router-dom";
 import Header from "../components/Header";
 
 function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as string;
   console.error(error);
 
   return (
-    <>
-    <header>
-    <nav className="navbar">
-            <div className="navbutton">
-                <NavLink
-                to="/"
-                >
-                    Home
-                </NavLink>
-                <NavLink
-                to="/expeditions"
-                >
-                    Expeditions
-                </NavLink>
-                <NavLink
-                to="/goals"
-                >
-                    Goals
-                </NavLink>
-                <NavLink
-                to="/locales"
-                >
-                    Locales
-                </NavLink>
-            </div>
-    </nav>
-    </header>
-    <Header />
-    <hr />
-      <main>
+    <main>
+      <nav className="navbar">
+          <div className="navbutton">
+            <NavLink
+            to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+            to="/expeditions"
+            >
+              Expeditions
+            </NavLink>
+            <NavLink
+            to="/goals"
+            >
+              Goals
+            </NavLink>
+            <NavLink
+            to="/locales"
+            >
+              Locales
+            </NavLink>
+          </div>
+      </nav>
+      <Header />
         <h1>You're in deep, uncharted territory now. Better play it safe and try another page!</h1>
-      </main>
-    </>
+    </main>
   );
 }
 
